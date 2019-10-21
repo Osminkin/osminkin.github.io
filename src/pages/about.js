e6621ccedb282340877e681fb2162de9e10db728
Osminkin <37698921+Osminkin@users.noreply.github.com>
2019-10-20 19:32:27 -0700

    test1

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const aboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Post's place</p>
+    </div>
+  )
+}
+
+export default aboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,13 @@
 import React from "react"
-import styles from "./about-css-modules.module.css"
-import Container from "../components/container"
 
-console.log(styles)
 
-const User = props => (
-  <div className={styles.user}>
-    <img src={props.avatar} className={styles.avatar} alt="" />
-    <div className={styles.description}>
-      <h2 className={styles.username}>{props.username}</h2>
-      <p className={styles.excerpt}>{props.excerpt}</p>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>It's Eugene Osminkin</h2>
     </div>
-  </div>
-)
+  )
+}
 
-export default () => (
-  <Container>
-    <h1>About CSS Modules</h1>
-    <p>CSS Modules are cool</p>
-    <User
-      username="Jane Doe"
-      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-    />
-    <User
-      username="Ivan Petrov"
-      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-      excerpt="I'm Ivan Petrov, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-    />
-  </Container>
-)
+export default IndexPage
